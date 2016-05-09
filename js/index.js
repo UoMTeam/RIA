269e330212ee32427642eb965c868aa5049cefaf
JIANGYIMING <jiangyiming@JIANGYIMINGs-MacBook-Pro.local>
2016-05-10 00:35:43 +0800

    a problem on chooseAll function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 /**
  * Created by jiangyiming on 5/9/16.
  */
-$(function(){
-    $('.form-control').datepicker({
+//$("document").ready(function () {
+//    $('#chooseAll').bind("click", function () {
+//        $("[name = title]:checkbox").attr("checked", true);
+//    });
+//    $('#NchooseAll').bind("click", function () {
+//        $("[name = title]:checkbox").attr("checked", false);
+//    });
+//});
+
+$("document").ready(function () {
+    $("[name = chooseAll]:radio").bind("click", function (event) {
+        if (event.target.id== "chooseAll") {
+            $("[name = title]:checkbox").attr("checked","checked");
+        } else {
+            $("[name = title]:checkbox").removeAttr("checked")
+        }
     });
 });
