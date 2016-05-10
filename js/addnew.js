a70d828b2efe6cd2bd82842049bb06d9497f5b52
JIANGYIMING <jiangyiming@JIANGYIMINGs-MacBook-Pro.local>
2016-05-11 01:13:55 +0800

    add radio questions

--- js/addnew.js
+++ js/addnew.js
@@ -15,5 +15,7 @@ $(function(){
     $("#addQ").click(function(){
         $("#addItems").slideToggle("slow");
     });
-
+    $("#addradio").click(function(){
+            $("#questions").prepend('<div><h2>Q1 单选题</h2><p><input id="1" type="radio" name="question1" /><input type="text" class="option" value="选项一" /><p><input id="2" type="radio" name="question1"><input type="text" class="option" value="选项二" /></div>');
+    });
 });
