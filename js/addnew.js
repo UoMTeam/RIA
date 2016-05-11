58bd23adbf22aaad869a86b81ef4c64030f07d4f
JIANGYIMING <jiangyiming@JIANGYIMINGs-MacBook-Pro.local>
2016-05-11 16:17:28 +0800

    add radio--first version

--- js/addnew.js
+++ js/addnew.js
@@ -2,13 +2,14 @@
  * Created by jiangyiming on 5/9/16.
  */
 $(function(){
-    $('#datetimepicker input').datepicker({
+    $('#datetimepicker').datepicker({
         format:'yyyy-mm-dd',
         weekStart: 1,
         todayBtn:  1,
         autoclose: 1,
         todayHighlight: true,
         showMeridian: 1
+        //pickerPosition:
     });
 
 
