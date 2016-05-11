a11e97f952a9334375a101d1bbbc3271e25d4628
JIANGYIMING <jiangyiming@JIANGYIMINGs-MacBook-Pro.local>
2016-05-11 18:28:57 +0800

    add publish function

--- js/addnew.js
+++ js/addnew.js
@@ -19,4 +19,8 @@ $(function(){
     $("#addradio").click(function(){
             $("#questions").prepend('<div><h4>Q1 单选题</h4><p><input id="1" type="radio" name="question1" /><input type="text" class="option" value="选项一" /><p><input id="2" type="radio" name="question1"><input type="text" class="option" value="选项二" /><div><a>上移</a><a>下移</a><a>复用</a><a>删除</a></div></div>');
     });
+    $("#push").click(function(){
+        $("#duetime").html( $("#datetimepicker").val());
+    });
 });
+
