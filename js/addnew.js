9d32cf8103bcf465810718fec79f6d9aa401412b
JIANGYIMING <jiangyiming@JIANGYIMINGs-MacBook-Pro.local>
2016-05-11 11:33:46 +0800

    add radios function

--- js/addnew.js
+++ js/addnew.js
@@ -16,6 +16,6 @@ $(function(){
         $("#addItems").slideToggle("slow");
     });
     $("#addradio").click(function(){
-            $("#questions").prepend('<div><h2>Q1 单选题</h2><p><input id="1" type="radio" name="question1" /><input type="text" class="option" value="选项一" /><p><input id="2" type="radio" name="question1"><input type="text" class="option" value="选项二" /></div>');
+            $("#questions").prepend('<div><h4>Q1 单选题</h4><p><input id="1" type="radio" name="question1" /><input type="text" class="option" value="选项一" /><p><input id="2" type="radio" name="question1"><input type="text" class="option" value="选项二" /><div><a>上移</a><a>下移</a><a>复用</a><a>删除</a></div></div>');
     });
 });
